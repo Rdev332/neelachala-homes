4e97debcdc4db770c33a5fbc6c0079a70c251b74
ashish-pcode <36891939+amgugu111@users.noreply.github.com>
2023-06-18 15:56:21 +0530

    added imports

--- src/components/Contact-with-map/index.jsx
+++ src/components/Contact-with-map/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import appData from '../../data/app.json'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const ContactWithMap = () => {
 
