6d826023360444f8d7aae04d22550898543ebf56
Sai Prasad Nayak <saiprasadnayak6858@gmail.com>
2023-07-28 00:12:03 +0530

    fetch all nested components for homepage data

--- src/pages/api/getHomePageData.js
+++ src/pages/api/getHomePageData.js
@@ -2,7 +2,7 @@
 // This function is used to get all projects from the CMS
 export default async function getHomePageData() {
     try{
-        const response = await fetch(`${process.env.NEXT_PUBLIC_CMS_URL}/home-page?populate=*`,{
+        const response = await fetch(`${process.env.NEXT_PUBLIC_CMS_URL}/home-page?populate[promotionPopup][populate]=*&populate[header_video][populate]=*&populate[header][populate]=*&populate[testimonials][populate]=*`,{
             headers: {
                 'Content-Type': 'application/json',
                 "Authorization": `bearer ${process.env.NEXT_PUBLIC_CMS_API_TOKEN}`,
