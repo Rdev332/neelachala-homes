38c10d0557d30a6b0e1de4953eae261358bf2272
amgugu111 <amgugu111@gmail.com>
2023-08-12 01:27:34 +0530

    added access banner

--- src/components/Skills-circle/index.jsx
+++ src/components/Skills-circle/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
-import skillsCircleData from '../../data/skills-circle.json'
+
+const features = [
+  {
+    id: 1,
+    title: "Affordable",
+    icon: "/assets/img/access/affordable.png",
+  },
+  {
+    id: 2,
+    title: "Quality",
+    icon: "/assets/img/access/quality.png",
+  },
+  {
+    id: 3,
+    title: "Sustainable",
+    icon: "/assets/img/access/sustainable.png",
+  },
+  {
+    id: 4,
+    title: "Inclusive",
+    icon: "/assets/img/access/inclusive.png",
+  },
+];
 
 const SkillsCircle = () => {
   const cpStyle = {
@@ -19,38 +39,40 @@ const SkillsCircle = () => {
   return (
     <section
       className="skills-circle section-padding bg-img parallaxie"
-      style={{ backgroundImage: "url(https://archo-react.vercel.app/assets/img/blog/3.jpg)", backgroundSize: "contain" }}
+      style={{ backgroundImage: "url(/assets/img/access/access-bg.jpg)" }}
       data-overlay-dark="7"
     >
       <div className="container">
-        <div className="section-head text-center">
+        <div className="section-head text-center mb-40">
           <div className="row justify-content-center">
             <div className="col-lg-6 col-md-8 col-sm-10">
               <h6 className="wow fadeInDown" data-wow-delay=".3s">
-                Coming Soon
+                Coming Soon!
               </h6>
               <h4 className="playfont wow flipInX" data-wow-delay=".5s">
                 Neelachala Access
               </h4>
+              <p>Every family deserves a home. 'Access' makes it possible.</p>
             </div>
           </div>
         </div>
         <div className="row wow fadeInUp" data-wow-delay=".3s">
-          {skillsCircleData.map((skill) => (
-            <div className="col-lg-3 col-md-6" key={skill.id}>
-              <div className="item text-center">
-                <div className="skill">
-                  <CircularProgressbar
-                    value={skill.val}
-                    strokeWidth={2}
-                    styles={cpStyle}
-                  />
-                  <span className="playfont">{`${skill.val}%`}</span>
-                </div>
-                <h5>{skill.name}</h5>
+          {features.map((feature) => (
+            <div className="col-lg-3 col-md-6 col-sm-6" key={feature.id}>
+              <div className="skill-icon">
+                <img src={feature.icon} alt="icon" />
+              </div>
+              <div className="skill-content">
+                <h5>{feature.title}</h5>
               </div>
             </div>
           ))}
+        </div>
+        <div className="row justify-content-center">
+          <a
+            className="btn-curve btn-bord btn-lit mt-40">
+            <span>Learn more</span>
+          </a>
         </div>
       </div>
     </section>
